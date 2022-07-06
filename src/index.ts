86f46ac982cd7664012e59039e971af5dd89537a
Jure Bogunovic <bogunovij@gmail.com>
2022-07-06 18:09:22 +0200

    exporting PluginWrapper

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export * from './PolywrapRemoteResolutionClient';
 export * from './RemoteUriResolver';
 export * from './PolywrapClientWithCustomCache';
 export * from './ExpirableWrapperCache';
+export * from './PluginWrapper';
